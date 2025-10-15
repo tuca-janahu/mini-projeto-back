3adf2bb1fecca130408ae154abaffd3eef4508b2
tuca-janahu <arturjanahu@gmail.com>
2025-10-15 08:35:07 -0300

    fix: deploy

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import db from "./database/configdb.ts"
+import db from "./database/configdb"
 
 // Load environment variables from .env file
 dotenv.config();
